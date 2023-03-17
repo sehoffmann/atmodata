7a2735d7e1edfc2128e4ab70e0c2d2e1dc5ddfbc
Sebastian Hoffmann <shoffmann.git@gmail.com>
2023-03-17 21:26:05 +0100

    fix: test

--- test/test_iter_tensor.py
+++ test/test_iter_tensor.py
@@ -1,21 +1,32 @@
 import unittest
 from expecttest import TestCase
+import functools
+import atmodata
 import torch
 from torchdata.datapipes.iter import IterableWrapper
 from atmodata.datapipes.iter.tensor import ThSplitter
 
+_assert_tensors_equal = functools.partial(torch.testing.assert_close, rtol=0, atol=0)
+
 class TestIterTensor(TestCase):
     def test_ThSplitter(self):
-        inp = [torch.arange(10*20).reshape(10,20), torch.arange(10*20).reshape(10,20)]
-        in_dp = IterableWrapper(inp)
+        inp = torch.arange(10*10).reshape(10,10)
+        in_dp = IterableWrapper([inp])
         
-        out_dp = ThSplitter(in_dp, 2, dim=0)
-        expected = [inp[0][:5], inp[0][5:], inp[1][:5], inp[1][5:]]
-        self.assertEqual(expected, list(out_dp))
+        # Test: basic
+        out_dp = ThSplitter(in_dp, 5, dim=0)
+        expected = [inp[:5], inp[5:]]
+        _assert_tensors_equal(expected, list(out_dp))
 
-        out_dp = ThSplitter(in_dp, 2, dim=1)
-        expected = [inp[0][:, :10], inp[0][:, 10:], inp[1][:, :10], inp[1][:, 10:]]
-        self.assertEqual(expected, list(out_dp))
+        # Test: dim argument
+        out_dp = ThSplitter(in_dp, 5, dim=1)
+        expected = [inp[:, :5], inp[:, 5:]]
+        _assert_tensors_equal(expected, list(out_dp))
+
+        # Test: partial chunks
+        out_dp = ThSplitter(in_dp, 4, dim=0)
+        expected = [inp[:4], inp[4:8], inp[8:]]
+        _assert_tensors_equal(expected, list(out_dp))
 
 if __name__ == '__main__':
     unittest.main()
